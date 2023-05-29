5781cf22cd9409df25eb150ecefe66860f4757e1
Eduardo Sobrino <esobrino@openk.com>
2023-05-29 08:59:58 -0400

    Sync all code on a new machine and updated the documentation based on the experience.

---
+++ Edam.Database/Scripts/Sql-Scripts/00000 - Add DbAdmin User.sql
@@ -0,0 +1,7 @@
+﻿USE [master]
+GO
+
+/* For security reasons the login is created disabled and with a random password. */
+/****** Object:  Login [openkadmin]    Script Date: 10/25/2016 10:20:07 AM ******/
+CREATE LOGIN [DbAdmin] WITH PASSWORD=N'#12!98z@l0k1t0!'
+GO
